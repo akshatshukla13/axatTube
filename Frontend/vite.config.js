a47d16990da6fe9d2682357de76eec5d1392c2fe
Akshat-Sh <akshatvijay1302@gmail.com>
2024-07-22 19:10:50 +0530

    new file:   Frontend/.env 	modified:   Frontend/vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,22 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+  build: {
+    outDir: 'dist'
   },
   server: {
     proxy: {
       '/api': {
-        target: 'https://backnnfront.vercel.app',
+        target: process.env.VITE_BACKEND_URL || 'http://localhost:3000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
-})
+});
 
+
+// https://backnnfront.vercel.app
