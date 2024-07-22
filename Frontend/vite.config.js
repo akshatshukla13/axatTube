23a8fc4c2c58d6cf3052a0741d9d701d5d6d0a69
Akshat-Sh <akshatvijay1302@gmail.com>
2024-07-22 19:17:29 +0530

    modified:   Frontend/package-lock.json 	modified:   Frontend/package.json 	modified:   Frontend/vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dotenv from "dotenv";
+import path from 'path';
+
+dotenv.config({ path: "./.env" });
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist'
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@app': path.resolve(__dirname, 'src/app'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+    },
   },
   server: {
     proxy: {
@@ -16,6 +28,3 @@ export default defineConfig({
     }
   }
 });
-
-
-// https://backnnfront.vercel.app
