6f29a86e276d78a11f282031269cbcb19b853d9c
Akshat-Sh <akshatvijay1302@gmail.com>
2024-11-05 23:36:59 +0530

    Refactor API proxy configuration in vite.config.js for improved clarity and update target URL

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,21 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
   server: {
     proxy: {
-      '/api': {
-        target: 'https://videotube-two.vercel.app/videos/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''), // This line can be removed if you want to send the request directly to the target URL
-      },
-    },
-  },
-});
+      // '/api' : "http://localhost:8000"
+      '/api' : "https://videotube-two.vercel.app"
+    }
+  }
+})
